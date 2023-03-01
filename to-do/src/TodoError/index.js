e8c192ee08b8c843c59327b52da1aeac15715d5c
cafalco96 <carlossfalco96@gmail.com>
2023-03-01 13:14:55 -0500

    delete to-do

--- to-do/src/TodoError/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function TodoError() {
-  return <p>It has an error, sorry ...</p>;
-}
-export { TodoError };
--- to-do/src/TodoTasksEmpty/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function TodoTasksEmpty() {
-  return <p>Please add tasks</p>;
-}
-export { TodoTasksEmpty };
